7e904732d3333c4de8c06eeb158fc7b198685581
rover33 <remy.j.pearlstone@gmail.com>
2017-11-13 01:54:59 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 var cardOne = cards[0];
-cardsInPlay.push("cardOne");
+cardsInPlay.push(cardOne);
 console.log("User flipped queen");
 var cardTwo = cards[2];
-cardsInPlay.push("cardTwo");
+cardsInPlay.push(cardTwo);
 console.log("User flipped king");
 
-if (cardsInPlay.length === 2); {
-    cardsInPlay[0] === cardsInPlay[1];
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
 } 
 else {

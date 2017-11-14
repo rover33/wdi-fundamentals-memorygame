ec8732f9da16581ea471367d2a34e01066944006
rover33 <remy.j.pearlstone@gmail.com>
2017-11-14 16:40:31 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -31,14 +31,27 @@ var checkForMatch = function () {
     }
 };
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+    cardId = this.getAttribute('data-id');
     cardsInPlay.push(cards[cardId].rank);
     console.log("User flipped" + ' ' + cards[cardId].rank);
     console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
 
+    this.setAttribute('src', cards[cardId].cardImage);
+
     checkForMatch();
 };
-flipCard(0)
-flipCard(2)
 
+var createBoard = function () {
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipcard);
+
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+};
+
+createBoard ();
